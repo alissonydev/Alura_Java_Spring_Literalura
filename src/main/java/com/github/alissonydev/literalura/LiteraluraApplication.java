dbb7980a098ad983ef799faff0d3f227d2fd1d62
alissonydev <alissonydev@outlook.com>
2026-02-19 20:53:36 -0300

    Menu

--- src/main/java/com/github/alissonydev/literalura/LiteraluraApplication.java
+++ src/main/java/com/github/alissonydev/literalura/LiteraluraApplication.java
@@ -1,13 +1,25 @@
 package com.github.alissonydev.literalura;
 
+import com.github.alissonydev.literalura.application.ApplicationMenu;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+    private final ApplicationMenu principal;
+
+    public LiteraluraApplication(ApplicationMenu principal) {
+        this.principal = principal;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        principal.showMenu();
+    }
 }
